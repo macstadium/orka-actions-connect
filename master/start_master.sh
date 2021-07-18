86978fb02711720bff9da4588678e488486cfa6d
jeff-vincent <jeff.d.vincent@gmail.com>
2021-07-18 11:41:37 -0700

    clean up

--- master/start_master.sh
+++ master/start_master.sh
@@ -1,9 +1,9 @@
 sudo docker run -d --restart always --name github-runner \
-  -e REPO_URL="<your-repo-url>" \
+  -e REPO_URL="https://github.com/jeff-vincent/orka-actions-spin-up" \
   -e RUNNER_NAME="master-runner" \
-  -e RUNNER_TOKEN="<your-runner-token>" \
-  -e RUNNER_WORKDIR="</tmp/github-runner-your-repo>" \
-  -e RUNNER_GROUP="<your-group>" \
+  -e RUNNER_TOKEN="AIJKXW6C6GNR7G7WB46H4TTA6N324" \
+  -e RUNNER_WORKDIR="/tmp/github-runner-your-repo" \
+  -e RUNNER_GROUP="your-group" \
   -e LABELS="master" \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v /tmp/github-runner-your-repo:/tmp/github-runner-your-repo \
