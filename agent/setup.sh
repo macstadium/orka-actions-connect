14d1deb19a0ad7e854e7a598d6a46d0f064b28a5
jeffdvincent <jeffdvincent@penguin.lxd>
2021-11-27 16:18:08 -0800

    major refactor

--- agent/setup.sh
+++ agent/setup.sh
@@ -2,6 +2,10 @@
 
 python3 -m pip install requests
 mkdir -p /Users/admin/agent
-cp runner_connect.py /Users/admin/agent/
-cp runner_connect.cfg /Users/admin/agent/
+cp connect.sh /Users/admin/agent/
+cp svc.sh /Users/admin/agent/
 sudo cp com.orka-actions-connect.plist /Library/LaunchDaemons/
+cd /Users/admin/agent/
+curl -o actions-runner-osx-x64-2.284.0.tar.gz \
+	-L https://github.com/actions/runner/releases/download/v2.284.0/actions-runner-osx-x64-2.284.0.tar.gz
+tar xzf ./actions-runner-osx-x64-2.284.0.tar.gz
