b25d48e1d257531e103f4c9f0c5665c9e3e0c50a
jeffdvincent <jeffdvincent@penguin.lxd>
2021-11-27 22:27:11 -0800

    WIP

--- agent/connect.sh
+++ agent/connect.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 export USER=$(curl -s "http://169.254.169.254/metadata/github_user" | \
-	python3 -c "import sys, json; print(json.load(sys.stdin)['value'])")
-export REPO=$$(curl -s "http://169.254.169.254/metadata/github_repo_name" | \
-	python3 -c "import sys, json; print(json.load(sys.stdin)['value'])")
+        python3 -c "import sys, json; print(json.load(sys.stdin)['value'])")
+export REPO=$(curl -s "http://169.254.169.254/metadata/github_repo_name" | \
+        python3 -c "import sys, json; print(json.load(sys.stdin)['value'])")
 
 vm_name=$(curl -s "http://169.254.169.254/metadata/vm_name" | \
-	python3 -c "import sys, json; print(json.load(sys.stdin)['value'])")
+        python3 -c "import sys, json; print(json.load(sys.stdin)['value'])")
 pat=$(curl -s "http://169.254.169.254/metadata/github_pat" | \
-	python3 -c "import sys, json; print(json.load(sys.stdin)['value'])")
+        python3 -c "import sys, json; print(json.load(sys.stdin)['value'])")
 repo_url="https://github.com/$USER/$REPO"
 
 runner_token=$(curl \
@@ -16,8 +16,9 @@ runner_token=$(curl \
 -H"Accept: application/vnd.github.v3+json" \
 -H"authorization: Bearer $pat" \
 "https://api.github.com/repos/$USER/$REPO/actions/runners/registration-token" | \
-	python3 -c "import sys, json; print(json.load(sys.stdin)['token'])")
+        python3 -c "import sys, json; print(json.load(sys.stdin)['token'])")
 
+cd /Users/admin/agent/
 ./config.sh --url $repo_url --token $runner_token --name $vm_name --unattended --work "_work" --labels $vm_name
 ./svc.sh install
 ./svc.sh start
