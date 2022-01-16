23e0f1ab1e3446e5ba62e88607fb92646240859a
jeffdvincent <jeffdvincent@penguin.lxd>
2022-01-16 15:18:50 -0800

    clean up

--- agent/setup.sh
+++ agent/setup.sh
@@ -4,7 +4,6 @@ python3 -m pip install requests
 mkdir -p /Users/admin/agent
 cp connect.sh /Users/admin/agent/
 cp svc.sh /Users/admin/agent/
-sudo cp com.orka-actions-connect.plist /Library/LaunchDaemons/
 cd /Users/admin/agent/
 curl -o actions-runner-osx-x64-2.284.0.tar.gz \
 	-L https://github.com/actions/runner/releases/download/v2.284.0/actions-runner-osx-x64-2.284.0.tar.gz
